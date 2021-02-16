983291205a38ec2eb7106eddf0792db143b73f30
BenSchZA <ben@bitsofether.com>
2021-02-16 11:54:14 +0200

    Update release

--- config/releases.exs
+++ config/releases.exs
@@ -1,6 +1,24 @@
 import Config
 
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
 config :app, AppWeb.Endpoint,
   server: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  http: [port: String.to_integer(System.get_env("PORT") || "4000")], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: secret_key_base
+
+# ## Using releases (Elixir v1.9+)
+#
+# If you are doing OTP releases, you need to instruct Phoenix
+# to start each relevant endpoint:
+#
+config :app, AppWeb.Endpoint, server: true
+#
+# Then you can assemble a release by calling `mix release`.
+# See `mix help release` for more information.
